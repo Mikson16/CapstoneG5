e2799807be66f772ebb5d67868ce29cf66d250ea
joseq <jquimi@uc.cl>
2025-11-09 21:03:10 -0300

    avance encontrar centro de objeto

--- ros2_ws/src/camera_pkg/camera_pkg/static_camera_papa.py
+++ ros2_ws/src/camera_pkg/camera_pkg/static_camera_papa.py
@@ -8,7 +8,7 @@ Nodo que recibe la imagen de la camara estatica y la procesa para identificar do
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import Image
-import cv2
+import cv2 as cv
 from cv_bridge import CvBridge
 from threading import Thread
 from time import sleep
@@ -29,6 +29,9 @@ class StaticCameraPapaNode(Node):
         # Crear suscriptor al mensaje de la camara estatica
         self.subscription = self.create_subscription(Image, 'static_camera/image_raw', self.queue_papa_callback, 10)
         self.get_logger().info('[Static Camera Papa Node]: Suscriptor creado')
+
+        # atributo de resultado
+        self.result = None
 
         # Bridge
         self.bridge = CvBridge()
@@ -66,7 +69,7 @@ class StaticCameraPapaNode(Node):
             try:
                 frame = self.img_q.get_nowait()
                 # Pasar la imagen al espacio de color HSV
-                frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+                frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV) 
 
                 # Definir rangos de color para detectar la bolsa de papa (amarillo), parametros ajustados a la iluminacino del lab
                 lower_yellow = np.array([30, 50, 80])
@@ -80,22 +83,23 @@ class StaticCameraPapaNode(Node):
                 upper_red_2 = np.array([179, 255, 255])
 
                 # Mascara Roja
-                mask_1 = cv2.inRange(frame_hsv, lower_red, upper_red) # imagen, rango bajo, rango alto
-                mask_2 = cv2.inRange(frame_hsv, lower_red_2, upper_red_2)
+                mask_1 = cv.inRange(frame_hsv, lower_red, upper_red) # imagen, rango bajo, rango alto
+                mask_2 = cv.inRange(frame_hsv, lower_red_2, upper_red_2)
 
-                mask = cv2.bitwise_or(mask_1, mask_2)
+                mask = cv.bitwise_or(mask_1, mask_2)
 
                 # Mascara amarilla
-                yellow_mask = cv2.inRange(frame_hsv, lower_yellow, upper_yellow)
+                yellow_mask = cv.inRange(frame_hsv, lower_yellow, upper_yellow)
 
 
                 # Aplicar la mascara a la imagen original
-                result = cv2.bitwise_and(frame, frame, mask=yellow_mask)
-                # self.find_center_papa(yellow_mask)
+                self.result = cv.bitwise_and(frame, frame, mask=yellow_mask)
+                center = self.find_center_papa(yellow_mask)
+                self.get_logger().info(f"El centro horizontal del objeto se encuentra en: {center}")
 
                 # Mostrar imagen para testeo
-                cv2.imshow('Static Camera Papa Detection', result)
-                cv2.waitKey(1)
+                cv.imshow('Static Camera Papa Detection', self.result)
+                cv.waitKey(1)
 
             except Empty:
                 frame = None
@@ -108,29 +112,72 @@ class StaticCameraPapaNode(Node):
         Encuentra el tramo promedio y retorna la posicion horizontal donde se encuentra el mayor promedio de pixeles que cumplen el objetivo
         """
         # La mascara ya es binaria, por lo cual la recibo
-        N, M = mask.shape
+        ### Metodo nuevo con funciones de opencv
+        try:
+            copy_mask = mask.copy()
+            contours, hierarchy = cv.findContours(copy_mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE) # RETR especifica los extremos exteriores del contorno y CHAIN es un metodo de aproximoacion por compresion vertical, horizontal y diagonal
+            largest = max(contours, key=cv.contourArea)
+            print(largest)
+            # if largest.all() == None:
+            #     return None
+            area = cv.contourArea(largest)
+            for ct in contours:
+                if area < 500:
+                    continue
 
-        length_list = []
-        mean_list = []
+                x, y, w, h = cv.boundingRect(cnt) # rectangulo
+                momento = cv.moments(ct)
+                if momento['m00'] != 0:
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
 
-        for fil in range(N):
-            col = mask[fil, : ] # para una fila tomo todas las columnas
-            white_pixels = np.where(col == 255)[0]
+                cv.drawContours(self.result, [ct], -1, (0,255,0), 2)
+                cv.circle(self.result, (cx,cy), 4, (0,0,255), -1)
+            cv.imshow('Find center Contorno', self.result)
+            cv.waitKey(1)
+        except Exception as e:
+            self.get_logger().error(f"[Find center error]: {e}")
+        return True
+        ## Testear y mejorar en laboratorio
+            
+        # ### Metodo anterior
+        # N, M = mask.shape
 
-            first_pixel = white_pixels[0]
-            last_pixel = white_pixels[-1]
+        # length_list = []
+        # mean_list = []
 
-            length = int(np.abs(first_pixel - last_pixel))
-            mean = (first_pixel + last_pixel) / 2
+        # for fil in range(N):
+        #     col = mask[fil, : ] # para una fila tomo todas las columnas
+        #     white_pixels = np.where(col == 255)[0]
+        #     if white_pixels.size == 0: #caso donde no haya ningun blanco
+        #         continue 
 
-            if length > 0:
-                length_list.append(length)
-                mean_list.append(mean)
+        #     first_pixel = white_pixels[0]
+        #     last_pixel = white_pixels[-1]
+
+        #     length = int(np.abs(first_pixel - last_pixel))
+        #     mean = (first_pixel + last_pixel) / 2
+
+        #     if length > 0:
+        #         length_list.append(length)
+        #         mean_list.append(mean)
         
-        mean_list = np.array(mean_list)
-        length_list = np.array(length_list)
-        meam_length = np.mean(length_list)
+        # mean_list = np.array(mean_list)
+        # length_list = np.array(length_list)
+        # meam_length = np.mean(length_list)
 
+        # standart_deviaton_length = np.std(length_list)
+
+        # mask_1 = length_list > meam_length + 2 *  standart_deviaton_length
+        # mask_2 = length_list > meam_length - 2 *  standart_deviaton_length
+
+        # mask = np.logical_and(mask_1, mask_2)
+
+        # filtered_means = np.array(mean_list[mask])
+
+        # mean_center = np.mean(filtered_means)
+
+        # return mean_center
             
 
         # cv2.imshow('Static Camera Papa Detection Gray Scale', mask)
