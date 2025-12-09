01bd4bc6a52ca77204e4de699b54a5c850d6a687
Mikson16 <83667835+Mikson16@users.noreply.github.com>
2025-12-09 20:40:51 -0300

    Refactor comments in img_coord_trans.py
    Updated comments and documentation for clarity and consistency.

--- ros2_ws/src/img_calc_pkg/img_calc_pkg/img_coord_trans.py
+++ ros2_ws/src/img_calc_pkg/img_calc_pkg/img_coord_trans.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Nodo ROBUSTO de Transformación de Coordenadas (Imagen -> Robot SCARA)
-Permite inversión de ejes, rotación y escalado preciso.
+Nodo de Transformación de Coordenadas (Imagen -> Robot SCARA)
+Permite inversion de ejes, rotacion y escalado preciso.
 """
 
 import rclpy
@@ -60,23 +60,21 @@ class BagCoordTransNode(Node):
         self.get_logger().info('[Transform Node]: Iniciado. Esperando coordenadas...')
         
         self.corrector = CorrectorCorr()
-        # --- 1. CALIBRACIÓN FÍSICA ---
+
         self.ALTURA_CAMARA_MM = 1140.0
         self.ALTURA_PAPA_MM = -100.0
         
         # Factor de escala (mm por pixel)
-        # Calculado con altura 115cm. Si el ancho real visto por la camara es ~56cm en 640px -> 0.875
+        # Calculado con altura 114cm
         self.scale = 0.877192 
 
-        # Centro de la imagen (Pixel)
-        self.IMG_CENTER_X = float(520/2) # Asumiendo resolución 640x480
+        # Centro de la imagen
+        self.IMG_CENTER_X = float(520/2) 
         self.IMG_CENTER_Y = float(480/2) 
 
-        # POSICIÓN DE LA CÁMARA RESPECTO AL ROBOT
-        # Mide con una regla desde la BASE DEL ROBOT hasta el punto en la mesa
-        # que cae justo en el centro de la imagen.
-        self.ROBOT_OFFSET_X = 310.0 # mm (Distancia X desde base a centro imagen)
-        self.ROBOT_OFFSET_Y = 286.0 # mm (Distancia Y desde base a centro imagen)
+
+        self.ROBOT_OFFSET_X = 310.0 
+        self.ROBOT_OFFSET_Y = 286.0 
 
         # Desplazamiento base
         self.desp_x = 0#- 30.0 # 3 cm
@@ -85,25 +83,22 @@ class BagCoordTransNode(Node):
         # FACTORES DE CORRECCION
         self.K_DISTORTION = 2.155e-5
 
-        # --- 2. CONFIGURACIÓN DE EJES (LA CLAVE DEL PROBLEMA) ---
-        # Cambia estos True/False observando los resultados
+        # Cambiar estos True/False observando los resultados
         
-        self.SWAP_XY = False      # Pon True si al moverte en X imagen, cambia el Y robot
-        self.INVERT_X = True      # Pon True si al ir a la derecha en imagen, el X robot baja
-        self.INVERT_Y = False     # Pon True si al ir abajo en imagen, el Y robot baja
-
-        # -----------------------------------------------------------
+        self.SWAP_XY = False      # True si al mover en Xen la imagen, cambia el Y robot
+        self.INVERT_X = True      # True si al ir a la derecha en la imagen, el X robot baja
+        self.INVERT_Y = False     # True si al ir abajo en la imagen, el Y robot baja
 
         # Publicadores y suscriptores
         self.publisher = self.create_publisher(Int16MultiArray, 'bag_coord_trans/new_coords', 10)
         self.subscriber = self.create_subscription(Int16MultiArray, 'static_camera/papa_coord', self.coord_callback, 10)
 
         # Para debug visual
-        self.debug_map_pub = None # Opcional: Podrías publicar la imagen de debug
+        self.debug_map_pub = None
     
     def coord_callback(self, msg):
         """
-        Se ejecuta cada vez que la cámara detecta algo.
+        Se ejecuta cada vez que la camara detecta algo.
         """
         try:
             # 1. Obtener coordenadas crudas (Pixeles)
@@ -116,7 +111,7 @@ class BagCoordTransNode(Node):
             # Comentar esta linea para no usar el corrector gaussiano
             u_corr, y_corr = self.corrector.corregir_corrd(raw_u, raw_v) 
 
-            # 2. Centrar coordenadas (Origen en el centro de la imagen)
+            # 2. Centrar coordenadas
             # u_centrado positivo = derecha del centro
             # v_centrado positivo = abajo del centro
             u_centered = (u_corr - self.IMG_CENTER_X) 
@@ -154,8 +149,7 @@ class BagCoordTransNode(Node):
 
 
 
-            # 4. Traslación al Origen del Robot
-            # Sumamos el offset para llevar el (0,0) de la imagen al (302, 286) del robot
+            # 4. Traslacion al Origen del Robot
             robot_x = int(self.ROBOT_OFFSET_X + x - self.desp_x)
             robot_y = int(self.ROBOT_OFFSET_Y + y - self.desp_y)
 
@@ -167,7 +161,7 @@ class BagCoordTransNode(Node):
             self.get_logger().info(f'\n Las coordenadas cartesianas a enviar son x {robot_x}, y {robot_y}')
             self.publisher.publish(out_msg)
 
-            # Visualización (Opcional, abre ventana en el PC del robot)
+            # Descomentar para visualizar
             # self.show_debug_window(robot_x, robot_y, raw_u, raw_v)
 
         except Exception as e:
@@ -215,185 +209,3 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
-
-
-# """
-# Nodo que recibe las coordenadas de la imagen y transforma los pixeles a las coordenadas del robot, luego envia la informacion apra realizar la cinematica inversa
-# """
-
-# import rclpy
-# from rclpy.node import Node
-# from sensor_msgs.msg import Image
-# from std_msgs.msg import Int16MultiArray
-# from threading import Thread
-# from time import sleep
-# import cv2
-# import numpy as np
-# from threading import Thread, Event
-# from queue import Queue, Empty, Full
-# import traceback
-# from math import atan2, degrees
-
-
-# class BagCoordTransNode(Node):
-#     def __init__(self):
-#         super().__init__('bag_coord_trans_node')
-#         self.get_logger().info(f'[Bag Coord Transformation Node]: ha sido iniciado')
-
-#         # Armar colas
-#         self.bag_coord_q = Queue(maxsize=1)
-#         self.stop_event = Event()
-
-#         # Publicadores y suscriptores
-#         self.publisher = self.create_publisher(Int16MultiArray, 'bag_coord_trans/new_coords', 10)
-
-#         self.subscriber = self.create_subscription(Int16MultiArray, 'static_camera/papa_coord', self.queue_coord_callback, 10)
-
-#         # Parametros
-#         self.origen_base = 0.0 # None # en radianes
-#         self.origen_eslabon = 0.0 # None # en radianes
-#         # Estos son 0 porque el origen del robot se considera el origen del sistema
-#         self.OFFSET_CAMARA_X = 302.0 # mm
-#         self.OFFSET_CAMARA_Y = 286.0 # mm
-
-#         self.factor_resolucion = 0.8797 # Calcular
-#         self.centro_camara_X = float(520/2)
-#         self.centro_camara_Y = float(480/2)
-
-#         # Para el grafico de debug
-#         self.map_width = 800
-#         self.map_height = 800
-#         self.scale_draw = 1.0 # 1 pixel = 1 mm para dibujar facil
-
-#         self.origin_draw_x = 750
-#         self.origin_draw_y = 50
-
-#         # Armar hilos
-#         self.processing_thread = Thread(target = self.processing_loop)
-#         self.processing_thread.start()
-
-
-#     def queue_coord_callback(self, msg):
-#         try:
-#             self.get_logger().info(f'Llego un mensaje, guardando en cola')
-#             data = msg.data
-#             self.bag_coord_q.put_nowait(data)
-
-#         except Full:
-#             self.get_logger().info(f'La cola esta llena')
-#         except Exception as e:
-#             self.get_logger().warning(f'Cola de bag coord con problema: {e}')
-#             self.get_logger().debug(traceback.format_exc())
-#     def draw_debug_map(self, robot_x, robot_y, raw_u, raw_v):
-#         """
-#         Dibuja un mapa 2D simulando la mesa vista desde arriba
-#         """
-#         # 1. Crear lienzo negro (o limpiar el anterior)
-#         canvas = np.zeros((self.map_height, self.map_width, 3), dtype=np.uint8)
-
-#         # 2. Dibujar Ejes del Robot (Desde el origen visual)
-#         # Eje X+ (Hacia la Izquierda fisica)
-#         cv2.arrowedLine(canvas, (self.origin_draw_x, self.origin_draw_y), 
-#                         (self.origin_draw_x - 100, self.origin_draw_y), (255, 255, 255), 2)
-#         cv2.putText(canvas, "X+ (Izq)", (self.origin_draw_x - 140, self.origin_draw_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
-
-#         # Eje Y+ (Hacia Abajo fisico)
-#         cv2.arrowedLine(canvas, (self.origin_draw_x, self.origin_draw_y), 
-#                         (self.origin_draw_x, self.origin_draw_y + 100), (255, 255, 255), 2)
-#         cv2.putText(canvas, "Y+ (Abajo)", (self.origin_draw_x, self.origin_draw_y + 120), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 1)
-
-#         # 3. Dibujar BASE ROBOT (0,0) - Cuadrado Verde
-#         cv2.rectangle(canvas, (self.origin_draw_x - 20, self.origin_draw_y - 20), 
-#                       (self.origin_draw_x + 20, self.origin_draw_y + 20), (0, 255, 0), -1)
-#         cv2.putText(canvas, "BASE ROBOT", (self.origin_draw_x - 40, self.origin_draw_y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
-
-#         # 4. Dibujar CENTRO CAMARA (Offset) - Cruz Amarilla
-#         # Recuerda: X crece a la izquierda, Y crece abajo en tu sistema fisico
-#         cam_draw_x = int(self.origin_draw_x - self.OFFSET_CAMARA_X)
-#         cam_draw_y = int(self.origin_draw_y + self.OFFSET_CAMARA_Y)
-        
-#         cv2.drawMarker(canvas, (cam_draw_x, cam_draw_y), (0, 255, 255), markerType=cv2.MARKER_CROSS, markerSize=20, thickness=2)
-#         cv2.putText(canvas, "CAM CENTER", (cam_draw_x + 10, cam_draw_y), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
-
-#         # 5. Dibujar LA PAPA (Objetivo) - Circulo Rojo
-#         # Transformamos coordenada mm a pixel de dibujo
-#         # X robot crece a izq -> restamos de origen
-#         target_draw_x = int(self.origin_draw_x - robot_x) 
-#         # Y robot crece abajo -> sumamos a origen
-#         target_draw_y = int(self.origin_draw_y + robot_y)
-
-#         # Linea desde robot a papa (Azul)
-#         cv2.line(canvas, (self.origin_draw_x, self.origin_draw_y), (target_draw_x, target_draw_y), (255, 100, 0), 1)
-        
-#         # El punto
-#         cv2.circle(canvas, (target_draw_x, target_draw_y), 10, (0, 0, 255), -1)
-        
-#         # Texto de coordenadas
-#         text_info = f"Coord: X={robot_x} mm, Y={robot_y} mm"
-#         text_pixel = f"Raw Px: ({raw_u:.0f}, {raw_v:.0f})"
-#         cv2.putText(canvas, text_info, (target_draw_x + 15, target_draw_y), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
-#         cv2.putText(canvas, text_pixel, (target_draw_x + 15, target_draw_y + 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200, 200, 200), 1)
-
-#         # Mostrar ventana
-#         cv2.imshow("Debug Coordenadas Robot", canvas)
-#         cv2.waitKey(1)
-
-#     def processing_loop(self):
-#         while not self.stop_event.is_set():
-#             try:
-#                 # obtener data de la cola
-#                 data_coord = self.bag_coord_q.get_nowait()
-#                 # self.get_logger().info(f'dato_coord es {data_coord}')
-#                 # Aqui hay que hacer la transformacion de los pixeles a las coordenadas del robot
-#                 x_cam = float(data_coord[0])
-#                 y_cam = float(data_coord[1])
-
-#                 x_rel_mm = (self.centro_camara_X - x_cam) * self.factor_resolucion
-#                 y_rel_mm = (y_cam - self.centro_camara_Y) * self.factor_resolucion
-#                 # trasladar el origen de la esquina a la base del robot
-#                 x = int(x_rel_mm + self.OFFSET_CAMARA_X)
-#                 y = int(y_rel_mm + self.OFFSET_CAMARA_Y)
-
-#                 # self.get_logger().info(f'Las coordenadas x, y del objeto en las coordenadas del robot es {x, y}')
-#                 # Ahora enviar el mensaje, hacer la publicacion
-#                 msg = Int16MultiArray()
-#                 msg.data = [x, y]
-#                 self.publisher.publish(msg)
-
-#                 # 4. DIBUJAR MAPA
-#                 # Solo dibujamos si estamos en un entorno gráfico
-#                 # try:
-#                 #     self.draw_debug_map(x, y, x_cam, y_cam)
-#                 # except Exception as e_draw:
-#                 #     self.get_logger().warning(f"No se puede dibujar (SSH sin X11?): {e_draw}")
-
-
-#             except Empty:
-#                 continue # completar
-#             except Exception as e:
-#                 self.get_logger().warning(f'Problemas en el loop de procesamiento: {e}')
-#                 self.get_logger().debug(traceback.format_exc())
-#                 continue
-
-#     def destroy_threads(self):
-#         """
-#         Destruir todos los hilos antes de destruir el nodo
-#         """
-#         self.get_logger().info('Deteniendo hilos de procesamiento')
-#         self.stop_event.set()
-#         try:
-#             self.processing_thread.join(timeout=1.0)
-#         except:
-#             pass
-#         self.get_logger().info('Hilos detenidos')  
-
-# def main(args=None):
-#     rclpy.init(args=args)
-#     bag_coord_trans_node = BagCoordTransNode()
-#     rclpy.spin(bag_coord_trans_node)
-#     bag_coord_trans_node.destroy_threads()
-#     bag_coord_trans_node.destroy_node()
-#     rclpy.shutdown()
-
-# if __name__ == '__main__':
-#     main()
